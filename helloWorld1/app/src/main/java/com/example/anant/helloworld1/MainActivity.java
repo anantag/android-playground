8994cc4d00f1a8b1e732be389f686d25d98bff7e
Anant Agarwal <anantag23@gmail.com>
2016-09-17 17:50:09 -0700

    Minor syncup from main client
    Reloading the project after a long time, just a normal sync up.

--- helloWorld1/app/src/main/java/com/example/anant/helloworld1/MainActivity.java
+++ helloWorld1/app/src/main/java/com/example/anant/helloworld1/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
